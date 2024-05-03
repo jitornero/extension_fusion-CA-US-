b16b45a93e5e7c85919c3e9c1e600ad7f69594a2
Juan Ignacio Tornero <you@example.com>
2024-05-03 00:25:54 -0300

    Options y Specs FORD USA fusionado

--- background.js
+++ background.js
@@ -1,16 +1,38 @@
-chrome.action.onClicked.addListener((tab) => {
-  console.log("background.js");
-  console.log(tab.id)
-  chrome.scripting.executeScript({
-    target : {tabId : tab.id},
-    files : [ "content.js" ],
+function getCurrentTab(section) {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  chrome.tabs.query(queryOptions, ([tab]) => {
+    if (chrome.runtime.lastError)
+    console.error(chrome.runtime.lastError);
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    send(tab, section);
   });
-  
-  // chrome.runtime.onMessage.addListener((message) => {
-  //   console.log("Received data from content script:", message.data);
-  //   // Do whatever you want with the data received from the content script.
-  //   // For example, you can store it, manipulate it, or send it to a server.
- 
-  // });
- 
+}
+
+function send (tab, section){
+    console.log("background.js");
+    console.log(tab.id)
+    chrome.scripting.executeScript({
+      target : {tabId : tab.id},
+      files : [ `${section}` ],
+    });
+};
+
+let section;
+
+chrome.runtime.onMessage.addListener(function(greeting){
+
+  console.log('chayyane received', greeting);
+  if (greeting.options == true){
+    section = "content_options.js";
+    console.log('options it is true');
+    getCurrentTab(section);
+  }
+  else {
+    section = "content_specs.js";
+    console.log('specs it is true');
+    getCurrentTab(section);
+  }
+
 });
+
+
--- popup.js
+++ popup.js
@@ -4,3 +4,22 @@ if (sendMessageId) {
     // do something
   };
 }
+
+const greeting = {
+  hello:"hello from popup",
+  options: true,
+}
+
+document.getElementById('options').addEventListener('click', function() {
+  console.log(' options Clicked!');
+
+  chrome.runtime.sendMessage(greeting);
+});
+
+document.getElementById('specs').addEventListener('click', function() {
+  console.log(' specs Clicked!');
+  greeting.options = false;
+  chrome.runtime.sendMessage(greeting);
+});
+
+console.log('hello chayyane')
